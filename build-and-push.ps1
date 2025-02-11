2876bd2765029bf08a9d47531998ae81ac6452f5
autumn-brough <78739123+autumn-heights@users.noreply.github.com>
2025-02-11 22:49:51 +0800

    Build and push latest changes - 2025-02-11 22:49:51

--- build-and-push.ps1
+++ build-and-push.ps1
@@ -1,16 +1,26 @@
-
 # Run the npm build command
 npm run build
 
-# Add all changes to git
-git add .
+# Zip the public directory
+Compress-Archive -Path "./public" -DestinationPath "./public.zip"
 
-# Get the current date and time
-$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+# Check if there are any changes to commit
+$changes = git status --porcelain
 
-# Commit the changes with a message including the timestamp
-git commit -m "Build and push latest changes - $timestamp"
+if ($changes) {
 
-# Push the changes to the remote repository
-git push origin main
+    # Add all changes to git
+    git add .
 
+    # Get the current date and time
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    # Commit the changes with a message including the timestamp
+    git commit -m "Build and push latest changes - $timestamp"
+
+    # Push the changes to the remote repository
+    git push origin main
+
+} else {
+    Write-Output "No changes to commit."
+}
